914e47a35cc4dfe2e144faeea5608db6096a3264
Julien Pinto Da Fonseca <julien.pintodafonseca@gmail.com>
2020-05-06 11:39:57 +0200

    minor changes

--- program/autotest/ori.s
+++ program/autotest/ori.s
@@ -1,13 +1,13 @@
 # TAG = ori
 	.text
 	addi x1, x0, 1
-	ori x31, x0, 0  #Test registre 0 ORI 0  / 0 ORI 0 = 0
-	ori x31, x1, 1  #Test registre 1 ORI 1  / 1 ORI 1 = 1
-	ori x31, x0, 1  #Test registre 0 ORI 1  / 0 ORI 1 = 1
-	ori x31, x1, 0  #Test registre 1 ORI 0  / 1 ORI 0 = 1
-	ori x31, x31, 0 #Test registre 31 ORI 0 / 1 ORI 0 = 1
-	ori x31, x0, 0  #Test registre 0 ORI 0  / 0 ORI 0 = 0
-	ori x31, x31, 1 #Test registre 31 ORI 1 / 0 ORI 1 = 1
+	ori x31, x0, 0  #Test registre 0 OR 0  / 0 OR 0 = 0
+	ori x31, x1, 1  #Test registre 1 OR 1  / 1 OR 1 = 1
+	ori x31, x0, 1  #Test registre 0 OR 1  / 0 OR 1 = 1
+	ori x31, x1, 0  #Test registre 1 OR 0  / 1 OR 0 = 1
+	ori x31, x31, 0 #Test registre 31 OR 0 / 1 OR 0 = 1
+	ori x31, x0, 0  #Test registre 0 OR 0  / 0 OR 0 = 0
+	ori x31, x31, 1 #Test registre 31 OR 1 / 0 OR 1 = 1
 
 	# max_cycle 50
 	# pout_start
