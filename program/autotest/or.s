e2b697b2abded730bae2613aa9241b96b8976fe7
Julien Pinto Da Fonseca <julien.pintodafonseca@gmail.com>
2020-05-06 11:22:31 +0200

    minor changes

--- program/autotest/or.s
+++ program/autotest/or.s
@@ -1,13 +1,13 @@
 # TAG = or
 	.text
 	addi x1, x0, 1
-	or x31, x0, x0  #Test registre 0 AND registre 0  / 0 AND 0 = 0
-	or x31, x1, x1  #Test registre 1 AND registre 1  / 1 AND 1 = 1
-	or x31, x0, x1  #Test registre 0 AND registre 1  / 0 AND 1 = 1
-	or x31, x1, x0  #Test registre 1 AND registre 0  / 1 AND 0 = 1
-	or x31, x31, x0 #Test registre 31 AND registre 0 / 1 AND 0 = 1
-	or x31, x0, x0  #Test registre 0 AND registre 0  / 0 AND 0 = 0
-	or x31, x31, x1 #Test registre 31 AND registre 1 / 0 AND 1 = 1
+	or x31, x0, x0  #Test registre 0 OR registre 0  / 0 OR 0 = 0
+	or x31, x1, x1  #Test registre 1 OR registre 1  / 1 OR 1 = 1
+	or x31, x0, x1  #Test registre 0 OR registre 1  / 0 OR 1 = 1
+	or x31, x1, x0  #Test registre 1 OR registre 0  / 1 OR 0 = 1
+	or x31, x31, x0 #Test registre 31 OR registre 0 / 1 OR 0 = 1
+	or x31, x0, x0  #Test registre 0 OR registre 0  / 0 OR 0 = 0
+	or x31, x31, x1 #Test registre 31 OR registre 1 / 0 OR 1 = 1
 
 	# max_cycle 50
 	# pout_start
