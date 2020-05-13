4642d44f591322fda92bc821273d65937f3882f1
Julien Pinto Da Fonseca <julien.pintodafonseca@gmail.com>
2020-05-13 13:03:58 +0200

    minor changes

--- program/autotest/beq.s
+++ program/autotest/beq.s
@@ -1,21 +1,21 @@
 # TAG = beq
     .text
-	lui x1, 11
-	lui x31, 12
-	beq x1, x31, goto_a 
-	lui x31, 1
+	addi x1, x0, 11
+	addi x31, x0, 12
+	beq x1, x31, goto_a # jcond invalide car 11 != 12
+	addi x31, x0, 1
 goto_a:
-	lui x1, 42
-	lui x31, 42
-	beq x31, x1, goto_b
-	lui x31, 6
+	addi x1, x0, 42
+	addi x31, x0, 42
+	beq x31, x1, goto_b # jcond valide car 42 == 42
+	addi x31, x0, 6
 goto_b:
-	lui x31, 0
+	addi x31, x0, 0
 	
     # max_cycle 50
     # pout_start
-    # 0000C000
-    # 00001000
-    # 0002A000
+    # 0000000C
+    # 00000001
+    # 0000002A
     # 00000000
     # pout_end
