9f2e17f12470388abfc78ccd4e80052d6a1a7204
Julien Pinto Da Fonseca <julien.pintodafonseca@gmail.com>
2020-05-20 10:54:48 +0200

    minor changes

--- program/autotest/lw.s
+++ program/autotest/lw.s
@@ -6,6 +6,7 @@
         lw   x31, 4(x1)         # rd <- mem[addr[var2]] / addr[var1] + 4 = addr[var2]
         addi  x1, x1, 4         # x1 <- addr[var2]      / addr[var1] + 4 = addr[var2]
         lw   x31, 0(x1)         # rd <- mem[addr[var2]]
+        lw   x31, -4(x1)        # rd <- mem[addr[var1]] / addr[var2] - 4 = addr[var1]
         lw   x31, 4(x1)         # rd <- mem[addr[var3]] / addr[var2] + 4 = addr[var3]
         lw   x31, 8(x1)         # rd <- mem[addr[var4]] / addr[var2] + 8 = addr[var4]
 
@@ -20,6 +21,7 @@
     # 11111111
     # 22222222
     # 22222222
+    # 11111111
     # 33333333
     # 44444444
     # pout_end
